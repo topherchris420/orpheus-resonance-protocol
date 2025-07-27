b5199762f3ae1488448eb011bc1fc2055b2455d5
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2025-07-27 11:38:14 -0400

    Update BiometricPanel.tsx

--- src/components/BiometricPanel.tsx
+++ src/components/BiometricPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'eact';
+import React, { useState } from 'react';
 import { Slider } from '@/components/ui/slider';
 import { Button } from '@/components/ui/button';
 
